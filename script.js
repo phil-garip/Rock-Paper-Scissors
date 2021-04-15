275699e582ca3dd5e4c0c0d5632bdf3c64943edb
Philip Garip <phil.garip@gmail.com>
2021-04-15 12:41:10 -0400

    added pseudocode

---
+++ script.js
@@ -0,0 +1,48 @@
+// Dependencies (HTML Elements, Libraries, etc.) =====================
+
+// Starting Data =====================================================
+  // wins = 0
+  // losses = 0
+  // ties = 0
+  // choices - r, p or s
+
+// Functions =========================================================
+  // get user choice 
+    // Prompt the user (please pick R, P or S)
+    // store the users choice
+  // get the computer's choice
+    // randomly choos a choice from the list of choices
+    // randomly generate an index
+    // get the letter from our choice list by the index
+    // store the computer choice
+  // check for a win
+    //TIES
+    //if user input === computer choice, return tie
+    // add a tie
+    // log "It's a Tie"
+    // show stats
+    //WINS
+      //if user = P and computer = R, return win for user and store in userWin
+      //if user = R and computer = S, return win for user and store in userWin
+      //if user = S and computer = P. return win for user and store in userWin
+      // add a win
+      // log "You won"
+      // show stats
+    //LOSSES
+    //if user = R and computer = S, return loss for user and store in userLoss
+    //if user = S and computer = R, return loss for user adn store in userLoss
+    //if user = P and computer = S, return loss for user and store in userLoss
+    // add a loss
+    // log "You lost"
+    // show stats
+  // show stats
+    // logs the value for each stat in an alert
+    // ask the user to play again?
+
+// User Interactions =================================================
+  // play the game
+  // get user choice
+  // get the computer choice
+  // check for a win
+  
+// Initialization ====================================================
